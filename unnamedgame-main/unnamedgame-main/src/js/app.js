4e2c1771bae2169acc373286d1c30ac4ea2be12c
AdisDev <adrianzilic1@outlook.com>
2023-03-12 15:28:24 +0100

    https://github.com/AdisDev/unnamedgame.git

--- unnamedgame-main/unnamedgame-main/src/js/app.js
+++ unnamedgame-main/unnamedgame-main/src/js/app.js
@@ -2,6 +2,7 @@ let playerX, playerY;
 let centerX, centerY;
 let playerR;
 let playerSpeed;
+let gamemode = 0;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
@@ -19,9 +20,17 @@ function setup(){
 }
 
 function draw(){
-    background(102, 204, 255)
+    if(gamemode == 0){
+        background(233, 233, 233);
+        text("Enter fullscreen mode!", centerX-50, centerY - 20);
+        text("Press enter to play!")
+    }
+    else if(gamemode == 1){
+        background(102, 204, 255)
 
-    player();
+        world();
+        player();
+    }
 }
 
 function mousePressed(){
@@ -29,13 +38,14 @@ function mousePressed(){
     fullscreen(!fs);
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
 function player(){
     fill(222, 13, 44)
-    rect(playerX, playerY, playerR, playerR, 15)
+
+    if(playerX < centerX - 250 || playerX > centerX + 250 || playerY < centerY - 250 || playerY > centerY + 250){
+        fill(51, 204, 51);
+    }
+
+    rect(playerX, playerY, playerR, playerR, 15);
 
     if(keyIsDown(87)){
         playerY -= playerSpeed;
@@ -49,4 +59,26 @@ function player(){
     if(keyIsDown(65)){
         playerX -= playerSpeed;
     }
+
+    if(playerX <= 0){
+        playerX = 50;
+    }
+    else if(playerX >= width){
+        playerX = width - 50;
+    }
+    if(playerY >= height){
+        playerY = height - 50;
+    }
+    else if(playerY <= 0){
+        playerY = 50;
+    }
+}
+
+function world(){
+    fill(255, 255, 102);
+    rect(centerX - 250, centerY - 150, 500, 500, 10);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
